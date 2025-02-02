1c4c3acf8c45e87d04c84aca12060f72a2809fed
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-02-02 22:08:13 +0800

    some changes

--- task_6.sql
+++ task_6.sql
@@ -1,9 +1,6 @@
--- Select the correct database
 USE alx_book_store;
 
--- Insert multiple rows into the customers table
-INSERT INTO customers (customer_id, customer_name, email, address) VALUES
-( 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
-( 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
-( 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
-
+INSERT INTO Customers (customer_name, email, address) VALUES
+('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
+('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
+('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
