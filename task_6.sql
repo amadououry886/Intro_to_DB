a135cfdfd8a6c19c41ea37d2ed2c97f4025aab0a
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-02-02 22:31:25 +0800

    some changes

--- task_6.sql
+++ task_6.sql
@@ -1,6 +1,6 @@
 USE alx_book_store;
 
-INSERT INTO Customers (customer_name, email, address) VALUES
+INSERT INTO customers (customer_name, email, address) VALUES
 ('Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
 ('Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
 ('Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
