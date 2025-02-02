dc142eb47e79fefa0c8ed7bf8c6e1d6af4404c6e
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-02-02 21:38:05 +0800

    some modification

--- task_4.sql
+++ task_4.sql
@@ -1,7 +1,9 @@
 -- Select the correct database
+-- Select the correct database
 USE alx_book_store;
 
--- Get full details of the 'books' table
+-- Get full details of the 'Books' table
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'Books';
+
