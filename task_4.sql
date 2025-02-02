5585ddebd7ded5bd0165b2eff72ddf623edaec70
Amadou Oury Diallo <gaoualmanjallow@gmail.com>
2025-02-02 21:35:40 +0800

    some changes

---
+++ task_4.sql
@@ -0,0 +1,7 @@
+-- Select the correct database
+USE alx_book_store;
+
+-- Get full details of the 'books' table
+SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books';
